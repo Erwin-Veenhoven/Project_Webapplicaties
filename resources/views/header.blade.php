a20b390216719d004dc1007cc1b75ab77e11bae1
Erwin-Veenhoven <erwinveenhoven2907@gmail.com>
2023-03-29 12:55:51 +0200

    Added header and footer

---
+++ resources/views/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul class="navbar">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li style="float: right;"><a href="">Login</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.layout')
+
+@section('content')
+
+    <section>
+        <h1>Homepagina</h1>
+    </section>
+
+@endsection
